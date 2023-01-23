645f11f761098b46b65dc9143f49403810d3db63
Florencio Britez <florenciobritez49@gmail.com>
2023-01-22 22:44:11 -0300

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <ButtonComp text="Add task" color="green" />
+    </header>
+</template>
+
+<script>
+    import ButtonComp from "./Button"
+    export default {
+        name: 'HeaderView',
+        props: {
+            title: String
+        }, 
+        components: {
+            ButtonComp
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px
+    }
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <TaskComp
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
+  </div>
+</template>
+
+<script>
+import TaskComp from "./Task";
+export default {
+  name: "TasksComp",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    TaskComp,
+  },
+  emits: ["delete-task, toggle-reminder"],
+};
+</script>
